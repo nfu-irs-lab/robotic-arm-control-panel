7cb79f53982707f4fd93585c9a72387936c3e3f3
ziteh <honmonoh@gmail.com>
2020-12-26 15:59:45 +0800

    增加不同電腦的Configuration預處理器

--- Configuration.cs
+++ Configuration.cs
@@ -1,4 +1,7 @@
-﻿namespace HIWIN_Robot
+﻿//#define CONFIG_1
+#define CONFIG_2
+
+namespace HIWIN_Robot
 {
     /// <summary>
     /// 可調整之設定。
@@ -9,18 +12,36 @@
         /// 手臂連線IP位置。<br/>
         /// 設定錯誤將會無法連線。
         /// </summary>
-        public const string ArmIP = "192.168.0.3";
+        public const string ArmIP =
+#if (CONFIG_1)
+            "192.168.0.3";
+#elif (CONFIG_2)
+            "192.168.0.3";
+
+#endif
 
         /// <summary>
         /// 藍牙連線COM Port。<br/>
         /// 設定錯誤將會無法連線。
         /// </summary>
-        public const string BluetoothCOMPort = "COM21";
+        public const string BluetoothCOMPort =
+#if (CONFIG_1)
+            "COM21";
+#elif (CONFIG_2)
+            "COM3";
+
+#endif
 
         /// <summary>
         /// 夾爪連線COM Port。<br/>
         /// 設定錯誤將會無法連線。
         /// </summary>
-        public const string GripperCOMPort = "COM12";
+        public const string GripperCOMPort =
+#if (CONFIG_1)
+            "COM12";
+#elif (CONFIG_2)
+            "COM12";
+
+#endif
     }
 }
