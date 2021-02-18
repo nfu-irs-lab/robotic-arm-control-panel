a90bbf128998aaca23afabca99af8f2c291f5283
ziteh <honmonoh@gmail.com>
2021-02-18 16:35:31 +0800

    Rename

---
+++ HIWIN_Robot/Others/ActionFlowHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiwinRobot.Others
+{
+    public struct ActionStruce
+    {
+        public Action Action;
+        public string Comment;
+        public string Name;
+    }
+
+    public class ActionFlowHandler
+    {
+        public List<ActionStruce> Actions;
+
+        public ActionFlowHandler()
+        {
+            Actions = new List<ActionStruce>();
+        }
+    }
+}
