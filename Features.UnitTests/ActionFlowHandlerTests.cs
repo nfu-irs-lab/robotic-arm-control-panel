b3e906944b5429a4b378ca1bee4c5151b0431c90
ziteh <honmonoh@gmail.com>
2021-02-20 20:36:58 +0800

    新增 ActionFlowHAndler Add 的一個UnitTest

--- Features.UnitTests/ActionFlowHandlerTests.cs
+++ Features.UnitTests/ActionFlowHandlerTests.cs
@@ -1,14 +1,26 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using NUnit.Framework;
 
 namespace Features.UnitTests
 {
-    [TestClass]
+    [TestFixture]
     public class ActionFlowHandlerTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Test]
+        public void Add_InputName_ExistInListView()
         {
+            // Arrange.
+            System.Windows.Forms.ListView listView = new System.Windows.Forms.ListView();
+            IMessage message = new EmptyMessage();
+            IActionFlowHandler actionFlow = new ActionFlowHandler(listView, message);
+
+            string name = "Test";
+
+            // Act.
+            actionFlow.Clear();
+            actionFlow.Add(name, () => { });
+
+            // Assert.
+            Assert.AreEqual(name, listView.Items[0].SubItems[1].Text);
         }
     }
 }
