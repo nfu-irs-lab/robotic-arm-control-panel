9669b91dbf7ba3ada65c50c1379a2165bbef1a6d
ziteh <honmonoh@gmail.com>
2021-02-20 20:43:36 +0800

    增加UnitTest for ActionFlowHandler

--- Features.UnitTests/ActionFlowHandlerTests.cs
+++ Features.UnitTests/ActionFlowHandlerTests.cs
@@ -22,5 +22,25 @@ namespace Features.UnitTests
             // Assert.
             Assert.AreEqual(name, listView.Items[0].SubItems[1].Text);
         }
+
+        [Test]
+        public void Add_InputName_TheFirstItemInListViewSelected()
+        {
+            // Arrange.
+            System.Windows.Forms.ListView listView = new System.Windows.Forms.ListView();
+            IMessage message = new EmptyMessage();
+            IActionFlowHandler actionFlow = new ActionFlowHandler(listView, message);
+
+            string name = "Test";
+
+            // Act.
+            actionFlow.Clear();
+            actionFlow.Add(name, () => { });
+            actionFlow.Add(name, () => { });
+            actionFlow.Add(name, () => { });
+
+            // Assert.
+            Assert.IsTrue(listView.Items[0].Selected);
+        }
     }
 }
