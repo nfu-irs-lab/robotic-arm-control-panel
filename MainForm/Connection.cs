81c33e8d8378eb2bd0024add89d849c92ba8b6bb
nrurobotlab.member <nfurobotlab.member@gmail.com>
2021-11-03 20:41:21 +0800

    Slice Connection

---
+++ MainForm/Connection.cs
@@ -0,0 +1,53 @@
+﻿using RASDK.Basic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainForm
+{
+    public partial class MainForm
+    {
+        /// <summary>
+        /// 進行連線。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_connect_Click(object sender, EventArgs e)
+        {
+            MessageHandler.Log("Connect click", LoggingLevel.Trace);
+
+            for (var i = 0; i < Devices.Count; i++)
+            {
+                Devices[i].Connect();
+            }
+
+            if (Arm.Connected)
+            {
+                Arm.Speed = GetUiSpeed();
+                Arm.Acceleration = GetUiAcceleration();
+                UpdateNowPosition();
+
+                SetButtonsState(true);
+            }
+        }
+
+        /// <summary>
+        /// 進行斷線。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button_disconnect_Click(object sender, EventArgs e)
+        {
+            MessageHandler.Log("Disconnect click", LoggingLevel.Trace);
+
+            for (var i = 0; i < Devices.Count; i++)
+            {
+                Devices[i].Disconnect();
+            }
+
+            SetButtonsState(false);
+        }
+    }
+}
