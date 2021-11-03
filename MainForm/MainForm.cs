b6b01714a2b27f808040388f664e7f003d042528
nrurobotlab.member <nfurobotlab.member@gmail.com>
2021-11-03 20:44:36 +0800

    Slice Gripper

--- MainForm/MainForm.cs
+++ MainForm/MainForm.cs
@@ -485,37 +485,6 @@ namespace MainForm
 
         #endregion - Others -
 
-        #region - 夾爪 -
-
-        /// <summary>
-        /// 夾爪控制器。
-        /// </summary>
-        private IGripperController Gripper;
-
-        /// <summary>
-        /// 進行夾爪動作。
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void button_gripper_action_Click(object sender, EventArgs e)
-        {
-            Gripper.Control(Convert.ToInt32(numericUpDown_gripper_position.Value),
-                            Convert.ToInt32(numericUpDown_gripper_speed.Value),
-                            Convert.ToInt32(numericUpDown_gripper_force.Value));
-        }
-
-        /// <summary>
-        /// 重置夾爪。
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void button_gripper_reset_Click(object sender, EventArgs e)
-        {
-            Gripper.Reset();
-        }
-
-        #endregion - 夾爪 -
-
 
         #region - 位置記錄 -
 
