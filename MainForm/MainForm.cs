b80dfa7f3ad909c41c54520393b14150cfc5ef2e
nrurobotlab.member <nfurobotlab.member@gmail.com>
2021-11-03 20:43:27 +0800

    Slice Camera

--- MainForm/MainForm.cs
+++ MainForm/MainForm.cs
@@ -597,46 +597,6 @@ namespace MainForm
 
         #endregion - 動作流程 -
 
-        #region - 攝影機 -
-
-        private IDSCamera Camera;
-
-        private void button_camera_choose_Click(object sender, EventArgs e)
-        {
-            Camera.ChooseCamera();
-        }
-
-        private void button_camera_close_Click(object sender, EventArgs e)
-        {
-            Camera.Disconnect();
-        }
-
-        private void button_camera_open_Click(object sender, EventArgs e)
-        {
-            Camera.Init();
-            var mode = checkBox_camera_freerun.Checked ? CaptureMode.FreeRun : CaptureMode.Stop;
-            // Camera.Open(mode);
-            Camera.Connect();
-        }
-
-        private void button_camera_setting_Click(object sender, EventArgs e)
-        {
-            Camera.ShowSettingForm();
-        }
-
-        private void button_camera_snapshot_Click(object sender, EventArgs e)
-        {
-            checkBox_camera_freerun.Checked = false;
-            Camera.ChangeCaptureMode(CaptureMode.Snapshot);
-        }
-
-        private void checkBox_camera_freerun_CheckedChanged(object sender, EventArgs e)
-        {
-            var mode = checkBox_camera_freerun.Checked ? CaptureMode.FreeRun : CaptureMode.Stop;
-            Camera.ChangeCaptureMode(mode);
-        }
-
-        #endregion - 攝影機 -
 
         #region - 其它 -
 
